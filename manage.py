309ea3e217e12f302a0ac8fb4e257b65e897152e
Volodymyr <vladimir.savchenko@gmail.com>
2019-09-20 20:37:34 +0200

    manage cons as well

--- manage.py
+++ manage.py
@@ -3,10 +3,17 @@ import requests
 import os
 import json
 import io
+import ast
 
 from minio import Minio
 from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
              BucketAlreadyExists)
+
+def get_minio():
+    return Minio('minio-internal.odahub.io',
+              access_key='minio',
+              secret_key=open(os.environ.get('HOME')+"/.minio").read().strip(),
+              secure=False)
 
 def run(inputs):
     fac = cwltool.factory.Factory()
@@ -25,14 +32,22 @@ def run(inputs):
 
     return result_json, open("crab.cwl","r").read()
 
+def load(bucket):
+    client = get_minio()
+    try:
+        result = client.get_object(bucket, 'result')
+        inputs = client.get_object(bucket, 'inputs')
+        meta = json.loads(client.get_object(bucket, 'meta').read())
+    except Exception as e:
+        raise
+
+    return result, meta['cwl']
+
+
 def store(meta, inputs, result_json, bucket_name):
 
     # Initialize client with an endpoint and access/secret keys.
-    client = Minio('minio-internal.odahub.io',
-              access_key='minio',
-              secret_key=open(os.environ.get('HOME')+"/.minio").read().strip(),
-              secure=False)
-
+    client = get_minio()
 
     try:
         try:
@@ -65,6 +80,29 @@ def store(meta, inputs, result_json, bucket_name):
         except ResponseError as err:
              print(err)
 
+def get_record():
+#inputs, results, bucket_name):
+    r=requests.post('http://fuseki.internal.odahub.io/dataanalysis/query',
+                   params=dict(query='''PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX dda: <http://ddahub.io/ontology/analysis#>
+
+SELECT * WHERE {
+  ?sub ?x ?y . 
+  ?sub rdfs:subClassOf dda:CalibrationWorkflow .
+} 
+LIMIT 10
+'''),
+        auth=requests.auth.HTTPBasicAuth("admin", open(os.path.join(os.environ.get('HOME'), '.jena-password')).read().strip())
+    )
+
+
+    print(r)
+
+    print(r.text)
+
+
 
 def create_record(inputs, results, bucket_name):
 
@@ -74,6 +112,8 @@ def create_record(inputs, results, bucket_name):
         entries+="\ndda:{k}Input rdfs:subClassOf dda:hasInput . ".format(k=k)
         entries+="\ndda:{bucket_name} dda:{k}Input \"{v}\" .".format(bucket_name=bucket_name, k=k, v=v)
 
+    entries+="\ndda:{bucket_name} dda:usesCalibrationCandle db:Crab_pulsar .".format(bucket_name=bucket_name, k=k, v=v)
+
     print(entries)
 
     r=requests.post('http://fuseki.internal.odahub.io/dataanalysis/update',
@@ -81,6 +121,10 @@ def create_record(inputs, results, bucket_name):
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX dda: <http://ddahub.io/ontology/analysis#>
+
+    PREFIX dbprop: <http://dbpedia.org/property/>
+    PREFIX db: <http://dbpedia.org/resource/>
+
 
     INSERT DATA
     { 
@@ -99,31 +143,39 @@ def create_record(inputs, results, bucket_name):
 # find entity to run in rdf/jena
 
 inputs=dict(
-        t1="2019-09-01T13:35:15",
-        t2="2019-09-19T13:35:15",
+        t1="2018-08-10T13:35:15",
+        t2="2018-09-19T13:35:15",
         nscw=5,
-        chi2_limit=1.2,
-        systematic_fraction=0.01,
+        chi2_limit=1.0,
+        systematic_fraction=0.02,
 )
 
 import hashlib
 
 hashdigest = hashlib.md5(json.dumps(inputs).encode()).hexdigest()
 
-
-#result_json, cwl_content = run(
-#    inputs
-#) # run with inputs in context
-# in reana?
-
-result_json=""
-
+get_record()
 
 bucket_name = "workflow-crab-"+hashdigest
 
-#
-#store(dict(name=bucket_name, notebook_url="https://github.com/volodymyrss/cc-crab/blob/master/crab.ipynb", cwl=cwl_content), inputs, result_json, bucket_name)
+print("loading", bucket_name)
 
-create_record(inputs, result_json, bucket_name)
+try:
+    r = load(bucket_name)
+except:
+    r = None
+
+if r is not None:
+    result_json, cwl_content = r
+else:
+    result_json, cwl_content = run(
+        inputs
+    ) # run with inputs in context
+    # in reana?
+
+    store(dict(name=bucket_name, notebook_url="https://github.com/volodymyrss/cc-crab/blob/master/crab.ipynb", cwl=cwl_content), inputs, result_json, bucket_name)
+
+    create_record(inputs, result_json, bucket_name)
+
 
 
